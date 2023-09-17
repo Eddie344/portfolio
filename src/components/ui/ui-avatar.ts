00eb29abffe871e82eab2c0cb8a564112e328204
Eddie344 <e.bazhenov@perenio.com>
2023-09-17 22:21:31 +0300

    create ui-avatar component

---
+++ src/components/ui/ui-avatar.ts
@@ -0,0 +1,31 @@
+import { LitElement, css, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('ui-avatar')
+export class UiAvatar extends LitElement {
+  @property({ type: String })
+  image: string;
+
+  render() {
+    return html`
+      <div class="my-avatar">
+        <img src="${this.image}" class="my-avatar__image" />
+      </div>
+    `;
+  }
+
+  static styles = css`
+    .my-avatar {
+      width: 210px;
+      height: 210px;
+      background-color: var(--my-avatar-bg-color);
+      border-radius: 9999px;
+      padding: 4px;
+    }
+    .my-avatar__image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  `;
+}
