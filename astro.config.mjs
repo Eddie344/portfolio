8b040e38b6887512244114d0432cdfec05a615bd
Eddie344 <e.bazhenov@perenio.com>
2023-09-18 09:19:54 +0300

    install astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
 import lit from '@astrojs/lit';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [lit()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+  }),
 });
